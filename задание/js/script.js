447225abc44e57acb1bf11a0f7095c9a69bc6eba
NikitaArt55 <82262739+NikitaArt55@users.noreply.github.com>
2021-04-13 02:23:27 +0600

    Add files via upload

--- задание/js/script.js
+++ задание/js/script.js
@@ -1,16 +1,29 @@
-let m_pos;
-function resize(e){
-    let parent = resize_el.parentNode;
-    let dx = m_pos - e.x;
-    m_pos = e.x;
-    parent.style.width = (parseInt(getComputedStyle(parent, '').width) - dx) + "px";
-}
+var isResizing = false,
+  lastDownX = 0;
 
-let resize_el = document.getElementById("resize");
-resize_el.addEventListener("mousedown", function(e){
-    m_pos = e.x;
-    document.addEventListener("mousemove", resize, false);
-}, false);
-document.addEventListener("mouseup", function(){
-    document.removeEventListener("mousemove", resize, false);
-}, false);
+$(function() {
+  var container = $('#container'),
+    left = $('#leftbar'),
+    right = $('#rightbar'),
+    handle = $('#resize');
+
+  handle.on('mousedown', function(e) {
+    isResizing = true;
+    lastDownX = e.clientX;
+    return false;
+  });
+
+  $(document).on('mousemove', function(e) {
+    // we don't want to do anything if we aren't resizing.
+    if (!isResizing)
+      return;
+
+    var offsetRight = container.width() - (e.clientX - container.offset().left);
+
+    left.css('right', offsetRight);
+    right.css('width', offsetRight);
+  }).on('mouseup', function(e) {
+    // stop resizing
+    isResizing = false;
+  });
+});
